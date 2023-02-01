a6fe3080685059f6978f16bd448019df280b8c8a
ROBERT TARUS <>
2023-02-01 16:43:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hi, welcome to Band name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name is " + city_name + " " + pet_name)
